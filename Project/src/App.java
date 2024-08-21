145937b2a01c30e2a1d354219eef4d6567f45f85
Augusto Leao <vitelliavl@gmail.com>
2024-08-21 17:26:09 -0300

    Just testing git commands

--- Project/src/App.java
+++ Project/src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Library!!");
+        String s = sc.nextLine();
+        
+
+        System.out.println("Hello " + s);
+
     }
 }
