1ac24701e4894dcedeb89b87784cb7957f98b1f7
ArthurSGA <arodrigues@sga.pucminas.br>
2024-08-23 23:28:44 -0300

    Cleaning

--- Project/src/App.java
+++ Project/src/App.java
@@ -1,13 +1,1 @@
-import java.util.Scanner;
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Library!!");
-        String s = sc.nextLine();
-        
-
-        System.out.println("Hello " + s);
-
-    }
-}
